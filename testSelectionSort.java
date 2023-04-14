1ebde9963e4687c2ee0f35b3f74f4f2225379954
Mohammed <moemoe@umich.edu>
2023-04-13 21:25:48 -0400

    Test mixed and duplicate methods updated

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -55,4 +55,45 @@ testDuplicates();
         int[] result = new SelectionSort() .basicSelectionSort(arr);
         assertArrayEquals(Sortedarr, result);
     }
+    public void testMixed(){
+    	int[] arr = new int[5];
+        arr[0] = 8;
+        arr[1] = -9;
+        arr[2] = 7;
+        arr[3] = -10;
+        arr[4] = 2;
+        
+        int[] Sortedarr = new int[5];
+        Sortedarr[0] = -10;
+        Sortedarr[1] = -9;
+        Sortedarr[2] = 2;
+        Sortedarr[3] = 7;
+        Sortedarr[4] = 8;
+        
+        /** Test data contains with both positive, negative and zeros **/
+        int[] result = new SelectionSort() .basicSelectionSort(arr);
+        assertArrayEquals(Sortedarr, result);
+    }
+    
+    public void testDuplicates(){
+    	int[] arr = new int[5];
+        arr[0] = 8;
+        arr[1] = 8;
+        arr[2] = 7;
+        arr[3] = 10;
+        arr[4] = 2;
+        
+        int[] Sortedarr = new int[5];
+        Sortedarr[0] = 2;
+        Sortedarr[1] = 7;
+        Sortedarr[2] = 8;
+        Sortedarr[3] = 8;
+        Sortedarr[4] = 10;
+        
+        /** Test data contains duplicates **/
+        int[] result = new SelectionSort() .basicSelectionSort(arr);
+        assertArrayEquals(Sortedarr, result);
+    }
+}
+
     
